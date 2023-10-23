9b96f71f4d14cce3ba68a62d2ba8207e5b86d66d
Andrew Cortis <Andrew.Cortis@mcast.edu.mt>
2023-10-23 09:28:11 +0200

    Finished Adjacency Matrix

--- GraphImplementations/AdjacencyMatrixGraphImplementation.cs
+++ GraphImplementations/AdjacencyMatrixGraphImplementation.cs
@@ -8,11 +8,11 @@ namespace GraphImplementations
 {
     public class AdjacencyMatrixGraphImplementation
     {
-        private int numberOfVertices;
-        private int[,] adjacencyMatrix;
+        private int numberOfVertices; // 1 space
+        private int[,] adjacencyMatrix; // once initialised, this will take up |V| x |V| space
 
-        private Dictionary<string, int> vertexNames = new Dictionary<string, int>();
-        private Dictionary<int, string> indexToVertexNameMapping = new Dictionary<int, string>();
+        private Dictionary<string, int> vertexNames = new Dictionary<string, int>(); // about |V| space
+        private Dictionary<int, string> indexToVertexNameMapping = new Dictionary<int, string>(); // about |V| space
         // alternative using jagged arrays
         //private int[][] adjacencyMatrix;
 
@@ -20,7 +20,7 @@ namespace GraphImplementations
         public AdjacencyMatrixGraphImplementation(int numberOfVertices)
         {
             this.numberOfVertices = numberOfVertices;
-            adjacencyMatrix = new int[numberOfVertices, numberOfVertices];
+            adjacencyMatrix = new int[numberOfVertices, numberOfVertices]; // this creates a square matrix with numberOfVertices squared places
             
             /* possible setup... might be improved
             adjacencyMatrix = new int[numberOfVertices][];
@@ -69,5 +69,82 @@ namespace GraphImplementations
             adjacencyMatrix[indexTwo, indexOne] = 1;
         }
 
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("graph G {");
+
+            for (int i = 0; i < numberOfVertices; i++)
+            {
+                for (int j = i+1; j < numberOfVertices; j++) // this avoids having double edges, because the graph is undirected with no reflexive edges!
+                {
+                    if (adjacencyMatrix[i, j] == 1) // look up the adjacency matrix, if the vertices are adjacent, then we have an edge
+                    {
+                        sb.AppendLine($"{indexToVertexNameMapping[i]}--{indexToVertexNameMapping[j]}");
+                    }
+                }
+            }
+
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+
+        public int Degree(string v)
+        {
+            int index = vertexNames[v]; // get the row/column of vertex V
+
+            int degree = 0;
+
+            // this loop will repeat itself numberOfVertices times (i.e. |V|)
+            for (int i = 0; i < numberOfVertices; i++)
+            {
+                // go through each and every item in the row
+                if (adjacencyMatrix[index, i] != 0)
+                {
+                    // find the non-zero elements
+                    // this indicates an adjacent vertex / edge
+                    degree++; // add 1 to the degree
+                }
+            }
+
+            return degree;
+        }
+
+        public bool IsAdjacent(string u, string v)
+        {
+            int indexOne = vertexNames[u]; // about constant time
+            int indexTwo = vertexNames[v]; // about constant time
+
+            // reading from / writing to a specific location in matrix is also constant time
+            if (adjacencyMatrix[indexOne, indexTwo] != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IList<string> GetAllAdjacencies(string v)
+        {
+            List<string> adjacencies = new List<string>();
+
+            int index = vertexNames[v]; // get the row/column of vertex V
+
+            // this loop will repeat itself numberOfVertices times (i.e. |V|)
+            for (int i = 0; i < numberOfVertices; i++)
+            {
+                // go through each and every item in the row
+                if (adjacencyMatrix[index, i] != 0)
+                {
+                    adjacencies.Add(indexToVertexNameMapping[i]); // amortised O(1)
+                }
+            }
+
+            return adjacencies;
+        }
     }
 }
--- GraphImplementations/Program.cs
+++ GraphImplementations/Program.cs
@@ -2,6 +2,27 @@
 
 AdjacencyMatrixGraphImplementation myGraph = new AdjacencyMatrixGraphImplementation(5);
 
+myGraph.NameVertex(0, "A");
+myGraph.NameVertex(1, "B");
+myGraph.NameVertex(2, "C");
+myGraph.NameVertex(3, "D");
+myGraph.NameVertex(4, "E");
+
+myGraph.AddEdge("A", "B");
+myGraph.AddEdge("A", "E");
+myGraph.AddEdge("B", "C");
+myGraph.AddEdge("B", "D");
+myGraph.AddEdge("B", "E");
+myGraph.AddEdge("C", "D");
+myGraph.AddEdge("D", "E");
+
+
+Console.WriteLine($"The degree of A is {myGraph.Degree("A")}");
+Console.WriteLine($"The degree of B is {myGraph.Degree("B")}");
+Console.WriteLine($"The degree of C is {myGraph.Degree("C")}");
+
+Console.WriteLine($"The adjacencies of B are {String.Join(", ", myGraph.GetAllAdjacencies("B"))}");
+Console.WriteLine(myGraph);
 int a = 0;
 a = a + 1;
 
